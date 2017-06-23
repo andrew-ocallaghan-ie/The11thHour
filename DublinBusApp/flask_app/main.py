c65e46d895c9d372a5cf8e6d10a996b9448314ae
Reggie Murphy <reggiemurphy7@gmail.com>
2017-06-23 11:12:55 +0100

    Removed pkl file to solve merge conflict. Website update in progress

--- DublinBusApp/flask_app/main.py
+++ DublinBusApp/flask_app/main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_cors import CORS
 from busData import BusDB
+from sklearn.externals import joblib
+import requests
+import traceback
+import datetime
+import csv
 
 # View site @ http://localhost:5000/
 # --------------------------------------------------------------------------#
@@ -9,12 +14,79 @@ app = Flask(__name__)
 # Enable Cross Origin Resource Sharing
 CORS(app)
 
+#--------------------------------------------------------------------------#
+def scrape_weather():
+    '''Returns a summary of the current weather from the Wunderground API'''
+    # API URI
+    api = 'http://api.wunderground.com/api'
+    # API Parameters
+    city = '/IE/Dublin'
+    app_id = '/0d675ef957ce972d'
+    api_type = '/hourly'
+
+    URI = api + app_id + api_type + '/q' + city + '.json'
+
+    # Loading Data
+    try:
+        req = requests.get(URI)
+        data = req.json()
+
+    except:
+        data = []
+        print(traceback.format_exc())
+
+    # Temperature
+    current_temp = data['hourly_forecast'][0]['temp']['metric']
+    # Rainfall
+    current_precipitation = data['hourly_forecast'][0]['qpf']['metric']
+
+    # Returning Summary
+    return (current_temp, current_precipitation)
+
+
+#--------------------------------------------------------------------------#
+def extract_holidays():
+    '''Returns a list of school holidays'''
+    stop_info = open('static/school_holidays_2017.csv', 'r')
+    reader = csv.reader(stop_info)
+
+    holidays = []
+
+    for row in reader:
+        holidays += row
+
+    print(holidays)
+
+
 
 # --------------------------------------------------------------------------#
 # Index Page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    predictor = joblib.load('static/rf_regressor.pkl')
+    extract_holidays()
 
+    if request.method == 'POST':
+        origin_stop_id = request.form['origin']
+        desination_stop_id = request.form['destination']
+        # weather = scrape_weather()
+        # current_temp = weather[0]
+        # current_rain = weather[1]
+        current_hour = datetime.datetime.now().hour
+        if datetime.datetime.now().weekday() < 5:
+            is_weekend = 0
+        else:
+            is_weekend = 1
+
+
+    # x will be a list of inputs that we give to the predictor: time, rain etc.
+    # we then run this through the predictor model to get a predicted delay
+    # to send to flask and display to the user
+
+    # x = [1, current_temp, current_rain, current_hour, 0]
+    # delay = predictor.predict(x)
+
+    # this needs to be changed to only return the delay value
     return render_template('index.html', **locals())
 
 
