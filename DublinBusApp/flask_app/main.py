27f7260c981662a0710e3196c63cc02fe6884c18
Reggie Murphy <reggiemurphy7@gmail.com>
2017-06-28 09:55:56 +0100

    App takes route input and displays stops on map

--- DublinBusApp/flask_app/main.py
+++ DublinBusApp/flask_app/main.py
@@ -67,10 +67,10 @@ def extract_holidays():
 def index():
     predictor = joblib.load('static/rf_regressor.pkl')
 
-
     if request.method == 'POST':
         origin_stop_id = request.form['origin']
         desination_stop_id = request.form['destination']
+        users_route = request.form['user_route']
         # weather = scrape_weather()
         # current_temp = weather[0]
         # current_rain = weather[1]
@@ -83,10 +83,6 @@ def index():
             is_school_holiday = 0
 
         current_weekday = datetime.datetime.now().weekday()
-        if current_weekday < 5:
-            is_weekend = 0
-        else:
-            is_weekend = 1
 
 
     # x will be a list of inputs that we give to the predictor: time, rain etc.
@@ -106,10 +102,18 @@ def index():
 #   - /api/routes/routenum     -> returns all stops associated with route
 # --------------------------------------------------------------------------#
 # --------------------------------------------------------------------------#
+
 # API - Returns JSON file with stop info for bus route.
 @app.route('/api/routes/<string:routenum>', methods=['GET'])
-def get_route_info(routenum):
-    return BusDB().route_info(routenum)
+def get_stop_info(routenum):
+    return BusDB().bus_stop_info(routenum)
+
+# --------------------------------------------------------------------------#
+
+
+@app.route('/api/all_routes/', methods=['GET'])
+def get_route_info():
+    return BusDB().bus_route_info()
 
 # --------------------------------------------------------------------------#
 # Setting app to run only if this file is run directly.
