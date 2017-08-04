fe6712b8a4acd97f827a627ba9c42567900e74ed
Unknown <andrew.ocallaghan@ucdconnect.ie>
2017-08-04 14:42:42 +0100

    comminting kates's main.py

--- DublinBusApp/flask_app/main.py
+++ DublinBusApp/flask_app/main.py
@@ -1,7 +1,7 @@
-#http://flask.pocoo.org/
+# http://flask.pocoo.org/
 from flask import Flask, render_template, request, g, flash, redirect, url_for, session, Markup
 
-#https://flask-cors.readthedocs.io/en/latest/
+# https://flask-cors.readthedocs.io/en/latest/
 from flask_cors import CORS
 
 from busData import api, dbi, everything
@@ -10,31 +10,31 @@ from busData import api, dbi, everything
 #     find_viable_stops, route_planner
 
 
-#https://docs.python.org/3/library/datetime.html
-import datetime
+# https://docs.python.org/3/library/datetime.html
+from _datetime import datetime
 
-#https://www.sqlalchemy.org/
+# https://www.sqlalchemy.org/
 from sqlalchemy import create_engine
 
-#https://wtforms.readthedocs.io/en/latest/
+# https://wtforms.readthedocs.io/en/latest/
 from wtforms import Form, StringField, PasswordField, validators
 
-#https://passlib.readthedocs.io/en/stable/
+# https://passlib.readthedocs.io/en/stable/
 from passlib.hash import sha256_crypt
 
-#http://pymysql.readthedocs.io/en/latest/index.html
+# http://pymysql.readthedocs.io/en/latest/index.html
 import pymysql
 
-#https://docs.python.org/3/library/functools.html
+# https://docs.python.org/3/library/functools.html
 from functools import wraps
 
-#http://www.pythonforbeginners.com/requests/using-requests-in-python
+# http://www.pythonforbeginners.com/requests/using-requests-in-python
 import requests
 
-#http://pandas.pydata.org/
+# http://pandas.pydata.org/
 import pandas as pd
 
-#http://scikit-learn.org/stable/
+# http://scikit-learn.org/stable/
 from sklearn.externals import joblib
 from tkinter.constants import CURRENT
 
@@ -50,40 +50,62 @@ app = Flask(__name__)
 # Enable Cross Origin Resource Sharing
 CORS(app)
 
+
 # --------------------------------------------------------------------------#
 
 # Class for form
 class RegisterForm(Form):
     name = StringField('Name', [validators.length(min=1, max=50)])
-    username  = StringField('Username', [validators.length(min=4, max=25)])
+    username = StringField('Username', [validators.length(min=4, max=25)])
     email = StringField('Email', [validators.length(min=4, max=50)])
     password = PasswordField('Password', [
         validators.DataRequired(),
-        validators.EqualTo('confirm', message = 'Passwords do not match')
-        ])
+        validators.EqualTo('confirm', message='Passwords do not match')
+    ])
     confirm = PasswordField('Confirm Password')
-    
 
 
 # --------------------------------------------------------------------------#
 # Index Page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    dbi().get_max_sequence(4, 0)
+    # dbi().get_max_sequence(4, 0)
     if request.method == 'POST':
         html = ""
         src = request.form['origin']
         dest = request.form['destination']
-        route_options = everything(src, dest)
+        now_arrive_depart_selection = request.form['now_arrive_depart']
+        if now_arrive_depart_selection == '0':
+            time = datetime.now()
+            date = time.date()
+            weekday = time.weekday()
+            hour = time.hour
+            min = time.minute
+            # This needs to be edited to be able to handle the two different use cases
+            # i.e. Arrive By & Depart At. This is a good base for taking in the code though
+        else:
+            time = request.form['time'].split(":")
+            date = request.form['date'].split("-")
+            year = int(date[0])
+            month = int(date[1])
+            day = int(date[2])
+            weekday = datetime(year, month, day).weekday()
+            hour = int(time[0])
+            min = int(time[1])
+            time = datetime(year, month, day, hour, min)
+
+        route_options = everything(src, dest, time)
+        print('something unique', route_options)
 
         '''list.join method to concat'''
-        for option in route_options.keys():
-            '''list . join method would be better'''
-            html += route_options[option]['html']
-        html = Markup(html)
+        # for option in route_options.keys():
+        #     '''list . join method would be better'''
+        #     html += route_options[option]['html']
+        # html = Markup(html)
         return render_template('route_options.html', **locals())
 
     return render_template('home.html', **locals())
+
 
 # --------------------------------------------------------------------------#
 # Search for Route Page
@@ -147,31 +169,30 @@ def register():
 
 # --------------------------------------------------------------------------#
 # User login page
-@app.route('/login', methods=['GET','POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        #Get Form Fields
+        # Get Form Fields
         username = request.form['username']
         password_candidate = request.form['password']
         print(username)
-        
+
         engine = get_db()
         sql = "SELECT * FROM users WHERE username = %s"
         result = engine.execute(sql, [username])
-        all_data=result.fetchall()
-        
+        all_data = result.fetchall()
+
         if len(all_data) > 0:
             # Get stored hash
             data = all_data[0]
             password = data['password']
-            
-        
-            #Compare passwords
+
+            # Compare passwords
             if sha256_crypt.verify(password_candidate, password):
-                #Passed
+                # Passed
                 session['logged_in'] = True
                 session['username'] = username
-                flash('Your are now logged in','success')
+                flash('Your are now logged in', 'success')
                 return redirect(url_for('myroutes'))
             else:
                 error = 'Invalid login'
@@ -180,6 +201,7 @@ def login():
             error = 'Username not found'
             return render_template('home.html', error=error)
     return render_template('login.html')
+
 
 # --------------------------------------------------------------------------#
 # Check if user logged in
@@ -191,7 +213,9 @@ def is_logged_in(f):
         else:
             flash('Unauthorized, Please login', 'danger')
             return redirect(url_for('login'))
+
     return wrap
+
 
 # --------------------------------------------------------------------------#
 # Dashboard
@@ -202,22 +226,23 @@ def myroutes():
     engine = get_db()
     sql = "SELECT * FROM like_stop WHERE username = %s"
     result = engine.execute(sql, [username])
-    all_data=result.fetchall()
-    
-    stopnamelist=[0] * len(all_data)
-    stopidlist=[0] * len(all_data)
-    #print(stopnamelist)
-    for a in range(0,len(all_data)):
-        stopidlist[a]=all_data[a][1]
-    Length=len(all_data)
-    for a in range(0,len(all_data)):
+    all_data = result.fetchall()
+
+    stopnamelist = [0] * len(all_data)
+    stopidlist = [0] * len(all_data)
+    # print(stopnamelist)
+    for a in range(0, len(all_data)):
+        stopidlist[a] = all_data[a][1]
+    Length = len(all_data)
+    for a in range(0, len(all_data)):
         print(all_data[a]['stop_id'])
-        #stopnamelist[a]=all_data[a]['stop_id']
+        # stopnamelist[a]=all_data[a]['stop_id']
         sql = "SELECT Stop_name FROM Stops WHERE Stop_ID = %s"
         stopnamelist[a] = engine.execute(sql, [all_data[a]['stop_id']]).fetchall()[0][0]
     print(stopnamelist)
     print(stopidlist)
-    return render_template('myroutes.html',**locals())
+    return render_template('myroutes.html', **locals())
+
 
 # --------------------------------------------------------------------------#
 # delete the stop
@@ -226,56 +251,58 @@ def myroutes():
 def delete():
     if request.method == 'POST':
         username = session['username']
-        print(username) 
+        print(username)
         stop_id = request.form['user_delete']
-        
-        print(stop_id,username)
+
+        print(stop_id, username)
         engine = get_db()
-        
+
         sql = "DELETE FROM like_stop WHERE username = %s AND stop_id = %s"
-        result = engine.execute(sql, [username,stop_id])
-            
+        result = engine.execute(sql, [username, stop_id])
+
         return redirect(url_for('myroutes'))
 
     return redirect(url_for('myroutes'))
+
 
 # --------------------------------------------------------------------------#
 # Logout
 @app.route('/logout')
 def logout():
     session.clear()
-    flash('You are now logged out','success')
+    flash('You are now logged out', 'success')
     return redirect(url_for('index'))
 
+
 # --------------------------------------------------------------------------#
-#user like function
+# user like function
 @app.route('/likestop', methods=['POST'])
 @is_logged_in
 def likestop():
-    
     if request.method == 'POST':
         stop_id = request.form['stopnum']
-    
-        username = session['username'] 
-        
-        print(stop_id,username)
+
+        username = session['username']
+
+        print(stop_id, username)
         engine = get_db()
-        
+
         sql = "SELECT * FROM like_stop WHERE username = %s AND stop_id = %s"
-        result = engine.execute(sql, [username,stop_id])
-        all_data=result.fetchall()
-        
-        if len(all_data) >0:
-            flash('You have already added the stop into Myroutes','danger')
+        result = engine.execute(sql, [username, stop_id])
+        all_data = result.fetchall()
+
+        if len(all_data) > 0:
+            flash('You have already added the stop into Myroutes', 'danger')
         else:
             sql = "INSERT INTO like_stop(username, stop_id) VALUES( %s, %s)"
             engine.execute(sql, (username, stop_id))
-            flash('Congrats! you have added this stop into Myroutes','success')
-            
+            flash('Congrats! you have added this stop into Myroutes', 'success')
+
         stop_num = stop_id
         return render_template('bus_stop.html', **locals())
 
     return render_template('stop_search.html', **locals())
+
 
 # =================================== API ==================================#
 # An API is used to allow the website to dynamically query the DB without
@@ -292,12 +319,14 @@ def likestop():
 def get_route_info():
     return api().bus_route_info()
 
+
 # --------------------------------------------------------------------------#
 
 
 @app.route('/api/routes/<string:routenum>/<string:direction>/', methods=['GET'])
 def get_stop_info(routenum, direction):
     return api().bus_stop_info_for_route(routenum, direction)
+
 
 # --------------------------------------------------------------------------#
 
@@ -306,6 +335,7 @@ def get_stop_info(routenum, direction):
 def get_all_info():
     return api().all_stop_info()
 
+
 # --------------------------------------------------------------------------#
 
 
@@ -313,16 +343,20 @@ def get_all_info():
 def get_all_stop_info():
     return api().all_bus_stop_info()
 
-#--------------------------------------------------------------------------#
+
+# --------------------------------------------------------------------------#
 @app.route("/api/bike_data")
 def get_bikes():
     bikes = dbi().bikes()
     return json.dumps(bikes)
 
 
+
 # =================================== DB ==================================#
 
-URI="bikesdb.cvaowyzhojfp.eu-west-1.rds.amazonaws.com"
+
+
+URI = "bikesdb.cvaowyzhojfp.eu-west-1.rds.amazonaws.com"
 PORT = "3306"
 DB = "All_routes"
 USER = "teamgosky"
@@ -334,15 +368,17 @@ def connect_to_database():
     engine = create_engine(db_str.format(USER, PASSWORD, URI, PORT, DB), echo=True)
     return engine
 
+
 def get_db():
     engine = getattr(g, 'engine', None)
     if engine is None:
         engine = g.engine = connect_to_database()
     return engine
 
+
 # =================================== DB ==================================#
 
 # Setting app to run only if this file is run directly.
 if __name__ == '__main__':
-    app.secret_key='secret123'
+    app.secret_key = 'secret123'
     app.run(debug=True)
