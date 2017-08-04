04b10319d95d495abae74122b35d5a10f79dfe1a
Unknown <andrew.ocallaghan@ucdconnect.ie>
2017-08-04 14:33:08 +0100

    commitng kates busdata.py

--- DublinBusApp/flask_app/busData.py
+++ DublinBusApp/flask_app/busData.py
@@ -123,7 +123,7 @@ class api:
 
     def all_stop_info(self):
         engine = get_db()
-        sql = "SELECT Address, Lat, Lon, Category, Colour FROM All_routes.dublinbike_dart_luas;"
+        sql = 'SELECT Address, Lat, Lon, Category, Colour,Stop_Id FROM All_routes.dublinbike_dart_luas1;'
         result = engine.execute(sql)
         all_data = result.fetchall()
         stops = []
@@ -134,7 +134,8 @@ class api:
                 'latitude': float(row[1]),
                 'longitude': float(row[2]),
                 'category': row[3],
-                'color': row[4]
+                'color': row[4],
+                'stop_id':row[5]
             }
             stops.append(stop)
 
@@ -172,14 +173,17 @@ class dbi:
         Returns route, stop IDs, direction, seq in a list
         Todo include the stop names and max stop seq and scheduled number of stops to return the scheduled speed
         """
+
         src_lat, src_lon = self.location_from_address(src)
         dest_lat, dest_lon = self.location_from_address(dest)
+        self.mid_point_lat = (src_lat+dest_lat)/2
+        self.mid_point_lon = (src_lon +dest_lon)/2
         engine = get_db()
         radius = 0.3
 
         """use 6371 as constant and drop degree conv."""
-        sql = "SELECT  start_route, start_direction, start_stop, end_stop, start_stop_seq, end_stop_seq, distance_in_km_start, distance_in_km_end\
-        FROM(SELECT DISTINCT s.Route as start_route, s.Direction as start_direction, s.Stop_ID as start_stop, s.Stop_sequence as start_stop_seq, 111.111 *\
+        sql = "SELECT  start_route, start_direction, start_stop, end_stop, start_stop_seq, end_stop_seq, distance_in_km_start, distance_in_km_end, start_stop_name, end_stop_name\
+        FROM(SELECT DISTINCT s.Route as start_route, s.Direction as start_direction, s.Stop_ID as start_stop, s.Stop_sequence as start_stop_seq, s.Stop_name as start_stop_name, 111.111 *\
                 DEGREES(ACOS(COS(RADIANS(%s))\
         * COS(RADIANS(s.Lat))\
         * COS(RADIANS(%s - s.Lon))\
@@ -188,7 +192,7 @@ class dbi:
                     FROM All_routes.new_all_routes s\
                     HAVING distance_in_km_start< %s) As Start\
                     JOIN\
-        (SELECT DISTINCT e.Route as end_route, e.Direction as end_direction , e.Stop_ID as end_stop, e.Stop_sequence as end_stop_seq, 111.111 * \
+        (SELECT DISTINCT e.Route as end_route, e.Direction as end_direction , e.Stop_ID as end_stop, e.Stop_sequence as end_stop_seq, e.Stop_name as end_stop_name, 111.111 * \
         DEGREES(ACOS(COS(RADIANS(%s))\
         * COS(RADIANS(e.Lat))\
         * COS(RADIANS(%s - e.Lon))\
@@ -201,31 +205,45 @@ class dbi:
         result = engine.execute(sql, (src_lat, src_lon, src_lat, radius, dest_lat, dest_lon, dest_lat, radius))
         all_data = result.fetchall()
 
+
         dataframe = pd.DataFrame(all_data,
                                  columns=["Route", "Direction", "Start_Stop_ID", "End_Stop_ID", "Start_Stop_Sequence",
-                                          "End_Stop_Sequence", "Distance_in_km_from_start", "Distance_in_km_from_end"])
-        print(dataframe)
-        return self.priority_options(dataframe)
+                                          "End_Stop_Sequence", "Distance_in_km_from_start", "Distance_in_km_from_end", "Start_Stop_Name", "End_Stop_Name"])
+        dataframe['mid_point_lat']=self.mid_point_lat
+        dataframe['mid_point_lon']=self.mid_point_lon
+        return self.priority_options(dataframe)[0]
 
     def priority_options(self, dataframe):
         #this is here to select the nearest of the subset returned by sql to the user - minimises total user walking distance
         dataframe['low_score'] = dataframe["Distance_in_km_from_start"]+dataframe["Distance_in_km_from_end"]
         dataframe = dataframe.loc[dataframe.groupby('Route').low_score.idxmin()]
-        print('priority dataframe', dataframe)
-        return self.dataframe_to_dict(dataframe)
+        #return self.dataframe_to_dict(dataframe)
+        self.start_stop_seq =dataframe.Start_Stop_Sequence
+        self.end_stop_seq = dataframe.End_Stop_Sequence
+        self.start_route =dataframe.Route
+        self.direction = dataframe.Direction
 
-    def dataframe_to_dict(self, dataframe):
-        #this converts the interesting routes to a dictionary
-        route_options = dataframe.transpose().to_dict()
-        i = 1
-        break_at = len(route_options)
-        for option in route_options:
-            if i > break_at:
-                break
-            route_options["Option " + str(i)] = route_options.pop(option)
-            i += 1
-        print (route_options)
-        return route_options
+        return (dataframe, self.extract_lat_lon_stops())
+
+    #--------------------------------------------------------------------------#
+
+    def extract_lat_lon_stops(self):
+        '''Returns lat lon of all stops on route'''
+        co_ords = []
+        engine = get_db()
+
+        sql ='SELECT lat, lon FROM All_routes.new_all_routes\
+        WHERE Stop_sequence >= %s AND Stop_sequence<= %s AND Route = %s AND Direction = %s;'
+
+        #result = engine.execute(sql, (self.start_stop_seq, self.end_stop_seq, self.start_route, self.direction))
+        result = engine.execute(sql, (int(self.start_stop_seq.values[0]), int(self.end_stop_seq.values[0]), int(self.start_route.values[0]), int(self.direction.values[0])))
+        all_co_ord_data = result.fetchall()
+
+        for row in all_co_ord_data:
+            co_ords.append({"lat": row[0], "lng": row[1]})
+            # print("row:",row)
+
+        return (co_ords)
 
     # --------------------------------------------------------------------------#
 
@@ -269,6 +287,7 @@ class dbi:
         data = (json.loads(r.text))
 
         return(data)
+
 
     # --------------------------------------------------------------------------#
     def extract_holidays(self):
@@ -314,23 +333,34 @@ class dbi:
         time = all_data[0]
         return time
 
+def dataframe_to_dict(dataframe):
+    #this converts the interesting routes to a dictionary
+    route_options = dataframe.transpose().to_dict()
+    # print('printing length of route options',len(route_options))
+    i = 1
+    break_at = len(route_options)
+    for option in route_options:
+        if i > break_at:
+            break
+        route_options["Option " + str(i)] = route_options.pop(option)
+        i += 1
+    print('route options in dbi page', route_options)
+    return route_options
 
-def everything(src, dest):
+def everything(src, dest, time):
     """Determines the journey time for each viable route option
     Returns a list of options ordered by their journey time"""
 
-    route_options = dbi().find_nearby_stops(src, dest)
-    current_time = datetime.datetime.now()
-    current_weekday = current_time.weekday()
-    current_hour = current_time.hour
-    current_min = current_time.minute
-    bit1 = "1" if (current_min > 15) else "0"
-    bit2 = "1" if (current_min > 30) else "0"
-    bit3 = "1" if (current_min > 45) else "0"
-    time_bin = str(current_hour) + bit1 + bit2 + bit3
+    weekday = time.weekday()
+    hour = time.hour
+    min = time.minute
+    bit1 = "1" if (min > 15) else "0"
+    bit2 = "1" if (min > 30) else "0"
+    bit3 = "1" if (min > 45) else "0"
+    time_bin = str(hour) + bit1 + bit2 + bit3
 
-    current_date = current_time.date()
-    if current_date in dbi().extract_holidays():
+    date = time.date()
+    if date in dbi().extract_holidays():
         is_school_holiday = 1
     else:
         is_school_holiday = 0
@@ -338,28 +368,76 @@ def everything(src, dest):
     weather = dbi().scrape_weather()
     current_temp, current_wind = weather
 
-    for option in route_options.keys():
-        print(option)
-        '''todo add stop name to dict'''
-        direction = int(route_options[option]['Direction'])
-        route = route_options[option]['Route']
-        src_stop_seq = route_options[option]['Start_Stop_Sequence']
-        dest_stop_seq = route_options[option]['End_Stop_Sequence']
-        stops_to_travel = dest_stop_seq - src_stop_seq
-        '''consider mergine this with bigger call, overhead issue?'''
-        max_stop_seq = dbi().get_max_sequence(route, direction)
-        scheduled_time = dbi().get_sched_time(route, direction)
-        sched_speed_per_stop = scheduled_time / max_stop_seq
+    route_options = dbi().find_nearby_stops(src, dest)
+    route_options.Direction = route_options.Direction.astype(int)
+    route_options['Stops_To_Travel'] = route_options.End_Stop_Sequence - route_options.Start_Stop_Sequence
+    route_options['temp'] = current_temp
+    route_options['wind'] = current_wind
+    route_options['day'] = weekday
+    route_options['holiday'] = is_school_holiday
+    route_options['time_bin']=time_bin
+    route_options['time']=time
+
+    def sched_speed(dataframe):
+        direction = int(list(dataframe.Direction.unique())[0])
+        route = list(dataframe.Route.unique())[0]
+        dataframe['max_stop_sequence'] = dbi().get_max_sequence(route, direction)
+        dataframe['scheduled_time'] = dbi().get_sched_time(route, direction)
+        dataframe['sched_speed'] =  dataframe.scheduled_time / dataframe.max_stop_sequence
+        dataframe.drop(['max_stop_sequence','scheduled_time' ], axis=1)
+        return dataframe
+
+
+    route_options = route_options.groupby(['Route', 'Direction']).apply(sched_speed)
+
+
+
+    def everything_else(df):
+
+        def extra_time_bins(time):
+            times = []
+            time_bins = []
+            times_for_chart = []
+            for i in range(8):
+                times.append(time + datetime.timedelta(minutes=15 * i))
+
+            for time in times:
+                # makes time pretty
+                times_for_chart.append(str(time.hour) + ":" + str(time.minute))
+                # makes time bin for time
+                bit1 = "1" if (time.minute > 15) else "0"
+                bit2 = "1" if (time.minute > 30) else "0"
+                bit3 = "1" if (time.minute > 45) else "0"
+                '''str .join this later'''
+                time_bins.append(str(time.hour) + bit1 + bit2 + bit3)
+            return (time_bins, times_for_chart)
+
+        time_bins, pretty_times = extra_time_bins(time)
+        df = pd.concat([df] * 8, axis=0, ignore_index=True)
+
+        df['time_bin'] = time_bins
+        df['pretty_times'] = pretty_times
+
+        return df
+
+
+    route_options = route_options.groupby(['Route','Direction']).apply(everything_else)
+
+
+    def make_predictions(df):
+        route = list(df.Route.unique())[0]
         predictor = joblib.load('static/pkls/xbeta' + route + '.csvrf_regressor.pkl')
-        '''use lambda or zip to int all inputs, its cleaner'''
-        params = [
-            [int(current_weekday), int(time_bin), int(current_wind), int(current_temp), int(is_school_holiday),
-             int(sched_speed_per_stop), int(stops_to_travel), int(src_stop_seq)]]
+        columns = ['day', 'time_bin', 'wind', 'temp', 'holiday', 'sched_speed', 'Stops_To_Travel', 'Start_Stop_Sequence']
+        df['Predictions'] = predictor.predict(df[columns])
+        '''add stop name to df,'''
+        pred = str(df.Predictions.values[0])
 
-        time_pred = predictor.predict(params)[0]
-        print(time_pred, 'time_pred on main page')
-        route_options[option]['html'] = "<div data-toggle='collapse' data-target='#map'><div class='option_route' onclick='boxclick(this, 1)'>" + route + "</div><div class='option_src_dest'>" + str(
-            'i should be a name') + " to " + 'i too should be a name' + "</div><div class='option_journey_time'>" + str(
-            int(time_pred)) + "</div></div>"
+        #df['html'] = "<div data-toggle='collapse' data-target='#map'><div class='option_route' onclick='boxclick(this, 1)'>" + route + "</div><div class='option_src_dest'>" +str(df.Start_Stop_Name) + " to " + str(df.End_Stop_Name) + "</div><div class='option_journey_time'>" + pred + "</div></div>"
+        return df
 
-    return route_options
+    route_options = route_options.groupby(['Route', 'Direction']).apply(make_predictions)
+
+
+
+    return dataframe_to_dict(route_options)
+
