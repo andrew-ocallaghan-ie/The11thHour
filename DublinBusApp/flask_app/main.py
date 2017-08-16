c5191a5230fb05e62404120dc2ead8cd587361a3
Reggie Murphy <reggiemurphy7@gmail.com>
2017-08-16 14:09:50 +0100

    Fixed info windows. Other small changes

--- DublinBusApp/flask_app/main.py
+++ DublinBusApp/flask_app/main.py
@@ -63,27 +63,51 @@ class RegisterForm(Form):
 # Index Page
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if 'logged_in' in session:
-        username = session['username']
-        engine = get_db()
-        sql_home = "SELECT home FROM users WHERE username = %s"
-        result_home = engine.execute(sql_home, [username])
-        data_home = result_home.fetchall()
-        print(data_home[0][0])
-        sql_work = "SELECT work FROM users WHERE username = %s"
-        result_work = engine.execute(sql_home, [username])
-        data_work = result_work.fetchall()
-        print(data_work[0][0])
+    # if 'logged_in' in session:
+    #     username = session['username']
+    #     engine = get_db()
+    #     sql_home = "SELECT home FROM users WHERE username = %s"
+    #     result_home = engine.execute(sql_home, [username])
+    #     data_home = result_home.fetchall()
+    #     dest = data_home[0][0]
+    #     print(data_home[0][0])
+    #     sql_work = "SELECT work FROM users WHERE username = %s"
+    #     result_work = engine.execute(sql_home, [username])
+    #     data_work = result_work.fetchall()
+    #     dest = data_work[0][0]
+    #     print(data_work[0][0])
+
     if request.method == 'POST':
+
+        latitude = request.json.get('latitude')
+        longitude = request.json.get('longitude')
+        print(latitude)
+        print(longitude)
+
         if request.form['submit'] == 'normal':
             dest = request.form['destination']
         elif request.form['submit'] == 'work':
+            username = session['username']
+            engine = get_db()
+            sql_home = "SELECT home FROM users WHERE username = %s"
+            result_work = engine.execute(sql_home, [username])
+            data_work = result_work.fetchall()
             dest = data_work[0][0]
         elif request.form['submit'] == 'home':
+            username = session['username']
+            engine = get_db()
+            sql_home = "SELECT home FROM users WHERE username = %s"
+            result_home = engine.execute(sql_home, [username])
+            data_home = result_home.fetchall()
             dest = data_home[0][0]
 
+        if request.form['use_location']:
+            pass
+        print('value of use location', request.form['use_location'])
+
+
         src = request.form['origin']
-        dest = request.form['destination']
+
         now_arrive_depart_selection = request.form['now_arrive_depart']
         if now_arrive_depart_selection == '0':
             time = datetime.now()
